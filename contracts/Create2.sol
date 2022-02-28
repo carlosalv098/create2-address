c404360d7ba2beacdc65fa5f9a363296e6f759d6
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-28 15:21:18 -0600

    get_address() and get_bytecode()

--- contracts/Create2.sol
+++ contracts/Create2.sol
@@ -16,8 +16,20 @@ contract Create2Factory {
 
   Create2 public _newContract;
 
-  function deploy(uint _salt) external {
+  function deploy(uint256 _salt) external {
     _newContract = new Create2{salt: bytes32(_salt)}(msg.sender);
     emit Deploy(address(_newContract));
+  }
+
+  // function to get the address, this should match the address in the event
+
+  function get_address(bytes memory _bytecode, uint256 _salt) public view returns(address) {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(_bytecode)));
+    return address(uint160(uint(hash)));
+  }
+
+  function get_bytecode(address _owner) public pure returns(bytes memory) {
+    bytes memory bytecode = type(Create2).creationCode;
+    return abi.encodePacked(bytecode, abi.encode(_owner));
   }
 }
