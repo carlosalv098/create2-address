c642960da7a0e14db72f988b5466e34437afb032
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-27 19:49:31 -0600

    deploy

--- contracts/Create2.sol
+++ contracts/Create2.sol
@@ -9,3 +9,15 @@ contract Create2 {
     owner = _owner;
   }
 }
+
+contract Create2Factory {
+
+  event Deploy(address _addr);
+
+  Create2 public _newContract;
+
+  function deploy(uint _salt) external {
+    _newContract = new Create2{salt: bytes32(_salt)}(msg.sender);
+    emit Deploy(address(_newContract));
+  }
+}
